8932b3807fbbe667a663de75ec4044e14e9961ab
CalebHellmund <emailcaleb4college@gmail.com>
2025-07-13 17:05:41 -0400

    Functions to get from and add to clipboard added

--- ClipboardManager/ClipboardManager.cpp
+++ ClipboardManager/ClipboardManager.cpp
@@ -1,19 +1,55 @@
-// ClipboardManager.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <Windows.h>
 #include <iostream>
+#include <string>
 #include "CircularBuffer.h"
 
 using namespace std;
+void copyToClipboard(const wstring& tocopy);
+wstring getFromClipboard();
 
 int main(){
-    CircularBuffer<string> clipboard(2);
-    clipboard.addElement("Hello ");
-    clipboard.addElement("World");
-    cout << clipboard.getElement(0) << clipboard.getElement(1) << endl;
-    clipboard.addElement(", this works!");
-    cout << clipboard.getElement(0) << clipboard.getElement(1) << endl;
-
+    wstring test = L"This also works";
+    CircularBuffer<wstring> clipboard(2);
+    clipboard.addElement(L"Hello ");
+    clipboard.addElement(L"World");
+    wcout << clipboard.getElement(0) << clipboard.getElement(1) << endl;
+    clipboard.addElement(L", this works!");
+    wcout << clipboard.getElement(0) << clipboard.getElement(1) << endl;
+    copyToClipboard(test);
+    wcout << getFromClipboard() << endl;
     return 0;
+}
+void copyToClipboard(const wstring& tocopy) {
+    OpenClipboard(NULL);
+    EmptyClipboard();
+    size_t sizeInBytes = (tocopy.size() + 1) * sizeof(wchar_t);
+    HGLOBAL HG = GlobalAlloc(GMEM_MOVEABLE, sizeInBytes);
+    if (!HG) {
+        CloseClipboard();
+    }
+    else {
+        memcpy(GlobalLock(HG), tocopy.c_str(), sizeInBytes);
+        GlobalUnlock(HG);
+        SetClipboardData(CF_UNICODETEXT, HG);
+        CloseClipboard();
+    }
+}
+wstring getFromClipboard() {
+    OpenClipboard(NULL);
+    wstring input;
+    if (!IsClipboardFormatAvailable(CF_UNICODETEXT)) {
+        CloseClipboard();
+        return input;
+    }
+    HANDLE hData = GetClipboardData(CF_UNICODETEXT);
+    if (hData) {
+        LPCWSTR pText = static_cast<LPCWSTR>(GlobalLock(hData));
+        if (pText) {
+            input = pText;
+            GlobalUnlock(hData);
+        }
+    }
+
+    CloseClipboard();
+    return input;
 }
